3d63cfc3135127347a5506e4e90a1881b9719901
dsm23 <dsmurdoch357@gmail.com>
2019-10-17 23:15:26 +0100

    Setup simple webapp and mocked tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useAsync } from "react-use";
+
+import { API } from "./constants";
+
+import "./App.css";
+
+const fetchFn = async () => {
+  const response = await axios(API);
+  return response.data;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { loading, error, value: data } = useAsync(fetchFn, []);
+  return loading ? (
+    <div>loading...</div>
+  ) : error ? (
+    <div>error...</div>
+  ) : (
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { render, waitForElementToBeRemoved } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { API } from "./constants";
+
+import App from "./App";
+
+it("renders without crashing", () => {
+  const { baseElement } = render(<App />);
+
+  expect(baseElement).toBeTruthy();
+});
+
+it("axios mock success", async () => {
+  const mock = new MockAdapter(axios);
+
+  const objResponse = {
+    data: {
+      david: "test"
+    }
+  };
+
+  mock.onGet(API).reply(200, objResponse);
+
+  const { container, getByText } = render(<App />);
+
+  await waitForElementToBeRemoved(() => getByText("loading..."));
+
+  const selector = container.querySelector("pre");
+
+  expect(selector.textContent).toBe(JSON.stringify(objResponse, null, 2));
+});
+
+it("axios mock failure", async () => {
+  const mock = new MockAdapter(axios);
+
+  mock.onGet(API).reply(500);
+
+  const { container, getByText } = render(<App />);
+
+  await waitForElementToBeRemoved(() => getByText("loading..."));
+
+  const selector = container.querySelector("div");
+
+  expect(selector.textContent).toBe("error...");
 });
