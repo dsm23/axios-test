75450cb2f72b4d7ba85cba0d39cc58f113625d77
David Murdoch <dsmurdoch357@gmail.com>
2020-05-05 15:54:10 +0100

    Chore/update testing dependencies (#68)
    * Upgrade to @testing-library/react v10
    
    * update all dependencies
    
    * Update node versions in github actions

--- src/App.test.js
+++ src/App.test.js
@@ -18,15 +18,15 @@ it("axios mock success", async () => {
 
   const objResponse = {
     data: {
-      david: "test"
-    }
+      david: "test",
+    },
   };
 
   mock.onGet(API).reply(200, objResponse);
 
   const { container, getByText } = render(<App />);
 
-  await waitForElementToBeRemoved(() => getByText("loading..."));
+  await waitForElementToBeRemoved(getByText("loading..."));
 
   const selector = container.querySelector("pre");
 
